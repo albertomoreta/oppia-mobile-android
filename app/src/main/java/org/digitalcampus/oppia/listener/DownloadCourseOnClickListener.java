ba7ead8a927ca4fc4942be75970c99e2db9e2d09
Joseba <jjoseba@gmail.com>
2014-12-09 18:54:38 +0100

    Created a controller to manage asyncTasks during activity life cycle Implemented that controller in DownloadActivity

--- app/src/main/java/org/digitalcampus/oppia/listener/DownloadCourseOnClickListener.java
+++ app/src/main/java/org/digitalcampus/oppia/listener/DownloadCourseOnClickListener.java
@@ -3,6 +3,6 @@ package org.digitalcampus.oppia.listener;
 /**
  * Created by Joseba on 05/12/2014.
  */
-public interface DownloadCourseListClickListener {
+public interface DownloadCourseOnClickListener {
     public abstract void onClick(int position);
 }
