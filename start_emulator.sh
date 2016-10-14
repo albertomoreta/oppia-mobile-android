99959e1c2f4d96ba7a173cb9894e61860d9f9ec3
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-14 17:35:53 +0200

    Updated start_emulator.sh

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,5 +1,4 @@
-if [ "${TEST_SUITE}" = "connectedAndroidTest" ]; then
-	echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a
-	emulator -avd test -no-skin -no-audio -no-window &
-	android-wait-for-emulator
-	adb shell input keyevent 82 &  
+echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &   
