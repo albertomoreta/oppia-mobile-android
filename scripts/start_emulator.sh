ea9a997dde161fdba760a0dd7dc422d2d4ab56b9
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-17 17:54:33 +0200

    Use travis.yml matrix to test in multiple emulators

---
+++ scripts/start_emulator.sh
@@ -0,0 +1,11 @@
+if [ "${TEST_SUITE}" = "connectedAndroidTest" ]
+then
+	echo no | android create avd --force -n test -t $1 --abi $2
+	emulator -avd test -no-skin -no-audio -no-window &
+	android-wait-for-emulator
+	adb shell input keyevent 82 &
+	adb shell pm grant org.digitalcampus.mobile.learning android.permission.READ_PHONE_STATE
+  	adb shell pm grant org.digitalcampus.mobile.learning android.permission.READ_EXTERNAL_STORAGE
+  	adb shell pm grant org.digitalcampus.mobile.learning android.permission.WRITE_EXTERNAL_STORAGE
+fi
+   
