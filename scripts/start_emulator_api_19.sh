b69ff3fa0a47f8bd18d670e0649065fc9e83f130
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-17 17:13:09 +0200

    Changes for "wait-for-device" command

--- scripts/start_emulator_api_19.sh
+++ scripts/start_emulator_api_19.sh
@@ -2,7 +2,7 @@ if [ "${TEST_SUITE}" = "connectedAndroidTest" ]
 then
 	echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
 	emulator -avd test -no-skin -no-audio -no-window &
-	android-wait-for-emulator
+	adb -s emulator-5556 wait-for-device shell getprop init.svc.bootanim
 	adb -s emulator-5556 shell input keyevent 82 &
 	adb -s emulator-5556 shell pm grant org.digitalcampus.mobile.learning android.permission.READ_PHONE_STATE
   	adb -s emulator-5556 shell pm grant org.digitalcampus.mobile.learning android.permission.READ_EXTERNAL_STORAGE
