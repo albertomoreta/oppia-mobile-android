a502711d8bb30c0e2fbbda44b61f04692d529c71
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-18 13:40:33 +0200

    Changes on start_emulator.sh

--- scripts/start_emulator.sh
+++ scripts/start_emulator.sh
@@ -1,7 +1,7 @@
 if [ "${TEST_SUITE}" = "connectedAndroidTest" ]
 then
 	echo no | android create avd --force -n test -t $1 --abi $2
-	emulator -avd test -no-skin -no-audio -no-window &
+	emulator -avd test -no-audio &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
 	adb shell pm grant org.digitalcampus.mobile.learning android.permission.READ_PHONE_STATE
