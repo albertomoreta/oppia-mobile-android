65ab68cafc4dc1e9c5bfbd072abefe0e6f3fded6
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-10-22 11:15:08 +0200

    Added permision to dissable animations

--- scripts/start_emulator.sh
+++ scripts/start_emulator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "${TEST_SUITE}" = "connectedAndroidTest" ]
 then
-	echo no | android create avd --force -n test -t $1 --abi $2 --tag google_apis
+	echo no | android create avd --force -n test -t $1 --abi $2
 	emulator -avd test -no-audio -no-window &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
