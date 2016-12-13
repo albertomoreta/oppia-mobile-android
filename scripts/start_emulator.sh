32aa97dc6fdc7cfbcd9d8f37a621195712249f43
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-12-13 11:18:28 +0100

    Changes in travis.yml and start_emulator.sh

--- scripts/start_emulator.sh
+++ scripts/start_emulator.sh
@@ -5,8 +5,5 @@ then
 	emulator -avd test -no-skin -no-audio -no-window &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
-	adb shell pm grant org.digitalcampus.mobile.learning android.permission.READ_PHONE_STATE
-  	adb shell pm grant org.digitalcampus.mobile.learning android.permission.READ_EXTERNAL_STORAGE
-  	adb shell pm grant org.digitalcampus.mobile.learning android.permission.WRITE_EXTERNAL_STORAGE
 fi
    
