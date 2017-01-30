e3c4462fb83857f5d4731e3d24d65a68f2e904ec
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2017-01-30 12:36:41 +0100

    Force emulator with WXGA720 skin

--- scripts/start_emulator.sh
+++ scripts/start_emulator.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "${TEST_SUITE}" = "connectedAndroidTest" ]
 then
-	echo no | android create avd --force -n test -t $1 --abi $2
-	emulator -avd test -no-skin -no-audio -no-window &
+	echo no | android create avd --force -n test -t $1 --abi $2 --skin WXGA720
+	emulator -avd test -no-audio -no-window &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
 fi
