53f281892312c0d2dc012dc8b3f464521b917130
Alberto Moreta <alberto@digital-campus.org>
2017-05-30 10:45:23 +0200

    Added Register Intrumented unit tests

---
+++ app/src/androidTest/java/RegisterTest.java
@@ -0,0 +1,211 @@
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.activity.WelcomeActivity;
+import org.digitalcampus.oppia.api.MockApiEndpoint;
+import org.digitalcampus.oppia.listener.SubmitListener;
+import org.digitalcampus.oppia.model.User;
+import org.digitalcampus.oppia.task.LoginTask;
+import org.digitalcampus.oppia.task.Payload;
+import org.digitalcampus.oppia.task.RegisterTask;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import TestRules.DisableAnimationsRule;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class RegisterTest {
+
+    @Rule
+    public DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
+
+
+    private CountDownLatch signal;
+    private MockWebServer mockServer;
+    private Context context;
+    private Payload response;
+
+    @Before
+    public void setUp() throws Exception {
+        context = InstrumentationRegistry.getTargetContext();
+        signal = new CountDownLatch(1);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        signal.countDown();
+        mockServer.shutdown();
+    }
+
+    @Test
+    public void userRegister_OKResponse()throws Exception {
+        try {
+            mockServer = new MockWebServer();
+
+            String filename = "responses/response_201_register.json";
+
+            mockServer.enqueue(new MockResponse()
+                    .setResponseCode(201)
+                    .setBody(Utils.FileUtils.getStringFromFile(InstrumentationRegistry.getContext(), filename)));
+
+            mockServer.start();
+
+
+        }catch(IOException ioe) {
+            ioe.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        ArrayList<Object> users = new ArrayList<>();
+        User u = new User();
+        u.setUsername("");
+        u.setPassword("");
+        users.add(u);
+
+        Payload p = new Payload(users);
+        try {
+            RegisterTask task = new RegisterTask(context, new MockApiEndpoint(mockServer));
+            task.setRegisterListener(new SubmitListener() {
+                @Override
+                public void apiKeyInvalidated() { }
+
+                @Override
+                public void submitComplete(Payload r) {
+                    response = r;
+                    signal.countDown();
+                }
+            });
+            task.execute(p);
+
+            signal.await();
+
+            assertTrue(response.isResult());
+            assertEquals(context.getString(R.string.register_complete), response.getResultResponse());
+
+        }catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }catch(Exception e){}
+
+
+    }
+
+    @Test
+    public void userRegister_BadResponse()throws Exception {
+        try {
+            mockServer = new MockWebServer();
+
+            String filename = "responses/response_400_register.json";
+
+            mockServer.enqueue(new MockResponse()
+                    .setResponseCode(400)
+                    .setBody(Utils.FileUtils.getStringFromFile(InstrumentationRegistry.getContext(), filename)));
+
+            mockServer.start();
+
+
+        }catch(IOException ioe) {
+            ioe.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        ArrayList<Object> users = new ArrayList<>();
+        User u = new User();
+        u.setUsername("");
+        u.setPassword("");
+        users.add(u);
+
+        Payload p = new Payload(users);
+        try {
+            RegisterTask task = new RegisterTask(context, new MockApiEndpoint(mockServer));
+            task.setRegisterListener(new SubmitListener() {
+                @Override
+                public void apiKeyInvalidated() { }
+
+                @Override
+                public void submitComplete(Payload r) {
+                    response = r;
+                    signal.countDown();
+                }
+            });
+            task.execute(p);
+
+            signal.await();
+
+            assertFalse(response.isResult());
+
+        }catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }catch(Exception e){}
+
+
+    }
+
+    @Test
+    public void userRegister_Emptyesponse()throws Exception {
+        try {
+            mockServer = new MockWebServer();
+
+            mockServer.enqueue(new MockResponse()
+                    .setBody(""));
+
+            mockServer.start();
+
+
+        }catch(IOException ioe) {
+            ioe.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        ArrayList<Object> users = new ArrayList<>();
+        User u = new User();
+        u.setUsername("");
+        u.setPassword("");
+        users.add(u);
+
+        Payload p = new Payload(users);
+        try {
+            RegisterTask task = new RegisterTask(context, new MockApiEndpoint(mockServer));
+            task.setRegisterListener(new SubmitListener() {
+                @Override
+                public void apiKeyInvalidated() { }
+
+                @Override
+                public void submitComplete(Payload r) {
+                    response = r;
+                    signal.countDown();
+                }
+            });
+            task.execute(p);
+
+            signal.await();
+
+            assertFalse(response.isResult());
+            assertEquals(context.getString(R.string.error_processing_response), response.getResultResponse());
+
+        }catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }catch(Exception e){}
+
+
+    }
+
+}
